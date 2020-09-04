2f0672cf33048f674504143031eb17bdc02ceef4
Code1337 <35568583+D3codeV09@users.noreply.github.com>
2020-09-04 14:49:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Acc
+$(document).on("mouseenter", ".naccs .menu div", function() {
+	var numberIndex = $(this).index();
+
+	if (!$(this).is("active")) {
+		$(".naccs .menu div").removeClass("active");
+		$(".naccs ul li").removeClass("active");
+
+		$(this).addClass("active");
+		$(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
+
+		var listItemHeight = $(".naccs ul")
+			.find("li:eq(" + numberIndex + ")")
+			.innerHeight();
+		$(".naccs ul").height(listItemHeight + "px");
+	}
+});
